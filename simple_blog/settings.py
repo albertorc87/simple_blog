bb041a291c475c3db5df0f15ae01953713f707f5
Alberto <alberto.r.caballero.87@gmail.com>
2020-04-24 19:41:06 +0200

    Modificada la url

--- simple_blog/settings.py
+++ simple_blog/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/admin/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/tmp/')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
