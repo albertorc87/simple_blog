fa2b074dca31ab44b2a9e1822a4aecc574bf7ef4
Alberto <alberto.r.caballero.87@gmail.com>
2020-04-24 19:37:34 +0200

    Modificada ruta de root

--- simple_blog/settings.py
+++ simple_blog/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/admin/')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
